d330d37c18f855add639801cbabaef2aeece32ff
Gabriel Zapata <Gabriel Zapata@LAPTOP-SNP6N20D>
2017-04-22 19:29:22 -0500

    parte 1

---
+++ src/main/java/com/udea/miws/Figure.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.udea.miws;
+
+import java.io.StringWriter;
+import javax.json.Json;
+import javax.json.JsonObject;
+
+/**
+ *
+ * @author Usuario
+ */
+public class Figure {
+
+    public Figure(JsonObject json) {
+        this.json = json;
+    }
+    private JsonObject json;
+
+    public JsonObject getJson() {
+        return json;
+    }
+
+    public void setJson(JsonObject json) {
+        this.json = json;
+    }
+
+    @Override
+    public String toString() {
+     StringWriter writer=new StringWriter();
+     Json.createWriter(writer).write(json);
+     return writer.toString();
+    }
+    
+    
+}
